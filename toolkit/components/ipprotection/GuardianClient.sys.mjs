d91c9403b16045cc6cc03d0ac5db7badb71337d5
Sebastian Streich <streich.mobile@gmail.com>
2026-02-25 19:22:29 +0100

    Bug 2020725 - WIP Part 2: Provide a way for GeckoView to Authenticate in Guardian r=#ip-protection-reviewers
    In order to Authenticate to Guardian, we need firefox to provide a token.
    
    Our current IPPSigninWatcher has a Strong dependency on FxAClient that is not available in gecko view.
    So we replacee it with a GeckoViewIPPSignInWatcher, where
    fenix can provide an implementation that delivers tokens on-demand to guardian.
    
    Differential Revision: https://phabricator.services.mozilla.com/D285813

--- toolkit/components/ipprotection/GuardianClient.sys.mjs
+++ toolkit/components/ipprotection/GuardianClient.sys.mjs
@@ -750,7 +750,7 @@ XPCOMUtils.defineLazyPreferenceGetter(
   gConfig,
   "guardianEndpoint",
   "browser.ipProtection.guardian.endpoint",
-  "https://vpn.mozilla.com"
+  "https://vpn.mozilla.org"
 );
 XPCOMUtils.defineLazyPreferenceGetter(
   gConfig,
